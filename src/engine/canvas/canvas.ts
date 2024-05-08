b2d259e340aa6ed950cd445623767a8942fa19db
2246250681@qq.com <zhou6633>
2024-05-08 23:46:29 +0800

    canvas

--- src/engine/canvas/canvas.ts
+++ src/engine/canvas/canvas.ts
@@ -1,42 +1,103 @@
 import { ZObject } from "../object";
 
-class ZCanvas {
-    add(object: ZObject): ZCanvas {
+type Func = (obj: ZObject) => boolean | void
 
+class ZCanvas {
+    private _ctx!:CanvasRenderingContext2D
+    private _pix = window.devicePixelRatio
+    constructor() {
+        const canvas = document.createElement('canvas')
+        canvas.style.width = canvas.width + 'px'
+        canvas.style.height = canvas.height + 'px'
+        canvas.width = canvas.width * this._pix
+        canvas.height = canvas.height * this._pix
+        canvas.tabIndex = 0
+        this._ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+        const pick = (e: MouseEvent, event: string) => {
+            let {offsetX: x, offsetY: y} = e
+            x = x * this._pix
+            y = y * this._pix
+            this.eachReverse(obj => {
+                const rect = obj.getRect()
+                if (x >= rect.x1 && y >= rect.y1 && x <= rect.x2 && y <= rect.y2) {
+                    obj.emit(event as any)
+                    return true
+                }
+            })
+        }
+        canvas.addEventListener('mousemove', e => {
+            pick(e, 'move')
+        })
+        canvas.addEventListener('mousedown', e => {
+            pick(e, 'down')
+        })
+        canvas.addEventListener('mouseup', e => {
+            pick(e, 'up')
+        })
+        canvas.addEventListener('keydown', e => {
+            
+        })
+    }
+    private _lists: ZObject[] = []
+    each(func: Func) {
+        for (let i=0; i<this._lists.length; i++) {
+            if (func(this._lists[i])){
+                return
+            }
+        }
+        
+    }
+    eachReverse(func: Func) {
+        for (let i=this._lists.length - 1; i>=0; i--) {
+            if (func(this._lists[i])){
+                return
+            }
+        }
+    }
+    add(...objs: ZObject[]): ZCanvas {
+        objs.forEach(obj => {
+            obj.$setCtx(this._ctx)
+            this._lists.push(obj)
+        })
         return this
     }
     remove(object: ZObject): ZCanvas {
-
+        this._lists.splice(this._lists.indexOf(object), 1)
         return this
     }
     clear() {
-
+        this._lists.length = 0
     }
     contains(object: ZObject): boolean {
         return false
     }
     lists() {
-
+        return this._lists
     }
     mount(dom: HTMLElement) {
-
+        dom.appendChild(this._ctx.canvas)
     }
     unmount() {
-
+        this._ctx.canvas.parentElement?.removeChild(this._ctx.canvas)
     }
 
     redraw() {
-
+        this._ctx.clearRect(0, 0, this._ctx.canvas.width, this._ctx.canvas.height)
+        this._lists.forEach(obj => {
+            obj.redraw()
+        })
     }
 
-    setWidth() {
-
+    setWidth(w: number) {
+        this._ctx.canvas.width = w * this._pix
+        this._ctx.canvas.style.width = w + 'px'
     }
 
-    setHeight() {
-        
+    setHeight(h: number) {
+        this._ctx.canvas.height = h * this._pix
+        this._ctx.canvas.style.height = h + 'px'
     }
 
 }
 
-export {}
+export {ZCanvas}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ZPolygon } from './engine'
+import { ZCanvas } from './engine/canvas'
 import { ZCircle } from './engine/circle'
 import { ZEllipse } from './engine/ellipse'
 import { ZPolyline } from './engine/polyline'
@@ -7,66 +8,58 @@ import { ZTriangle } from './engine/triangle'
 import { ZTable } from './extends'
 import './style.css'
 
-const canvas = document.createElement('canvas')
-canvas.width = 500
-canvas.height = 500
-document.body.appendChild(canvas)
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 
 // table
 const table = new ZTable()
-table.$setCtx(ctx)
 table.setData('columns', [{key: "name", title: "名称"}, {key: "age", title: "年龄"}])
 table.setData('data', [{name: "zg", age: 27}, {name: 'blog', age: 18}])
 table.setWidth(100)
 table.setX(100)
-table.scale(1, 1)
 
 // polyline
 const polyline = new ZPolyline()
 polyline.setData('values', [[0, 0], [0.5, 0.5], [1, 0]])
-polyline.$setCtx(ctx)
-polyline.redraw()
 
 // polygon
 const polygon = new ZPolygon()
 polygon.setData('values', [[0, 0], [0.5, 0.5], [1, 0]])
 polygon.setData('fillColor', '#FF0000')
-polygon.$setCtx(ctx)
 polygon.setX(200)
 polygon.setY(200)
-polygon.redraw()
 
 // circle
 const circle = new ZCircle()
-circle.$setCtx(ctx)
 circle.setX(100)
 circle.setY(100)
 circle.setData('end', 270)
 circle.setData('fillColor', '#FF0000')
 circle.setData('connectCenter', false)
-circle.redraw()
 
 // ellipse
 const ellipse = new ZEllipse()
-ellipse.$setCtx(ctx)
 ellipse.setX(200)
 ellipse.setY(100)
 ellipse.setData('end', 270)
 ellipse.setData('fillColor', '#FF0000')
-ellipse.redraw()
 
 // rect
 const rect = new ZRect()
-rect.$setCtx(ctx)
 rect.setPosition(50, 200)
 rect.setData('round', true)
 rect.setData('fillColor', '#FF0000')
-rect.redraw()
 
 // triangle
 const triangle = new ZTriangle()
-triangle.$setCtx(ctx)
 triangle.setPosition(50, 300)
 triangle.setData('fillColor', '#FF0000')
-triangle.redraw()
+
+triangle.on('down', () => {
+    console.log('点击三角形')
+})
+
+const canvas = new ZCanvas()
+canvas.mount(document.body)
+canvas.setWidth(500)
+canvas.setHeight(500)
+canvas.add(table, polyline, polygon, triangle, rect, circle, ellipse)
+canvas.redraw()
